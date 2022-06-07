b1f3bcb732af1b48954c0965a69a25d963cb1fcd
dennisil <103424229+dennisil@users.noreply.github.com>
2022-06-07 14:43:00 +0300

    ppppp

---
+++ dist/seeders/chefs.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.chefs = void 0;
+const uuid_1 = require("uuid");
+exports.chefs = [
+    {
+        _id: (0, uuid_1.v4)(),
+        chef: "Yossi Shitrit",
+        dishes: [],
+        tags: [],
+    },
+    {
+        _id: (0, uuid_1.v4)(),
+        chef: "Aviv Moshe",
+        dishes: [],
+        tags: [],
+    },
+];
+//# sourceMappingURL=chefs.js.map
